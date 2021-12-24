b077b45631b5ef94681fa9f45a03589cadac75cc
benqui <bennyhoyos@hotmail.com>
2021-12-23 18:39:33 -0600

    reset p->04

--- programas/part_04/peliculas_app/android/app/src/main/kotlin/com/example/peliculas_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.peliculas_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- programas/part_04/pelis_app/android/app/src/main/kotlin/com/example/pelis_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pelis_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
