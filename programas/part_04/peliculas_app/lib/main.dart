82c049e963de9c03da832065e83e397c9b5072d5
benqui <bennyhoyos@hotmail.com>
2021-12-23 18:31:57 -0600

    testo

--- programas/part_04/peliculas_app/lib/main.dart
+++ programas/part_04/peliculas_app/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/screens/details_screen.dart';
+import 'package:peliculas_app/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'Peliculas App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
+      },
     );
   }
 }
